f61ac9a6ebe6b188b1d6de12d465345a64dadd9e
AmanKhys <amankhys2112@gmail.com>
2025-02-06 23:32:19 +0530

    save current unfinished progress on guest handlers, routes

--- internal/guest/handlers.go
+++ internal/guest/handlers.go
@@ -1,7 +1,6 @@
 package guest
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
 
@@ -22,30 +21,30 @@ func (g *Guest) LoginHandler(w http.ResponseWriter, r *http.Request) {
 	var user dto.UserLoginParams
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		log.Fatal("error decoding request json body: ", err)
+		log.Warn("error decoding request json body: ", err)
 		http.Error(w, "error decoding json request body", http.StatusBadRequest)
 		return
 	}
 
-	var ctx context.Context
+	var ctx = r.Context()
+	if user.Email == "" {
+		http.Error(w, "email not specified in json data", http.StatusBadRequest)
+		return
+	}
 	u, err := g.DB.GetUserByEmail(ctx, user.Email)
-
 	if err != nil {
-		log.Fatal("error taking item from database: ", err)
+		log.Warn("error taking item from database: ", err)
 		http.Error(w, "error fetching data from database", http.StatusInternalServerError)
 		return
 	}
-	if u.Email == user.Email && u.Password == user.Password {
+
+	if u.Email == "" {
+		http.Error(w, "no user found", http.StatusBadRequest)
+		return
+	} else if u.Email == user.Email && u.Password == user.Password {
 		w.Write([]byte("you've been logged in"))
 		return
-	} else if u.Email == user.Email {
-		w.Write([]byte("wrong password"))
-		return
-	} else {
-		w.Write([]byte("unable to login due to invalid fields"))
-		return
 	}
-
 }
 
 func (g *Guest) SignUpHandler(w http.ResponseWriter, r *http.Request) {
--- internal/guest/routes.go
+++ internal/guest/routes.go
@@ -12,9 +12,10 @@ var DB = db.New(dbConn)
 var g = Guest{DB: DB}
 
 func RegisterRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("GET /", g.HomeHandler)
+	mux.HandleFunc("GET /home", g.HomeHandler)
 	// mux.HandleFunc("GET /login/", g.LoginPageHandler)
 	mux.HandleFunc("POST /login", g.LoginHandler)
 	// mux.HandleFunc("GET /signup/", g.SignUpPageHandler)
 	mux.HandleFunc("POST /signup", g.SignUpHandler)
+
 }
