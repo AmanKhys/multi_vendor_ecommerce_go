9daa7bd64cc29abf3e3e1198acfca07e41be7f0b
AmanKhys <amankhys2112@gmail.com>
2025-02-06 23:35:57 +0530

    remove ReadCookie() from utils/session.go

--- pkg/utils/session.go
+++ pkg/utils/session.go
@@ -2,8 +2,6 @@ package utils
 
 import (
 	"net/http"
-
-	log "github.com/sirupsen/logrus"
 )
 
 // Write SessionID cookie function
@@ -17,20 +15,4 @@ func WriteCookie(w http.ResponseWriter, SessionID string) {
 		Secure:   true,
 		SameSite: http.SameSiteLaxMode,
 	})
-}
-
-// Reead SessionID cookie function
-func ReadCookie(r *http.Request, name string) (*http.Cookie, error) {
-	cookie, err := r.Cookie(name)
-
-	if cookie == nil {
-		return nil, http.ErrNoCookie
-	}
-	if err != nil {
-		log.Warnf("error fetching cookie %s", name)
-		http.Error(w, "error fetching cookie", http.StatusInternalServerError)
-		return
-	}
-
-	return cookie, nil
 }
