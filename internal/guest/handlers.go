5f8efb21a4b867844d4a08d86c783a157e9292a5
AmanKhys <amankhys2112@gmail.com>
2025-02-15 16:56:13 +0530

    make Oauth config field for guest; make handler functions to oauth login for google auth users and add the routes;

--- internal/guest/handlers.go
+++ internal/guest/handlers.go
@@ -16,13 +16,15 @@ import (
 	"github.com/amankhys/multi_vendor_ecommerce_go/repository/db"
 	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/oauth2"
 )
 
 var RoleSeller = "seller"
 var RoleUser = "user"
 
 type Guest struct {
-	DB *db.Queries
+	DB     *db.Queries
+	config *oauth2.Config
 }
 
 func (g *Guest) HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -59,9 +61,7 @@ func (g *Guest) UserSignUpHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Info("reached here mf")
 	hashed, err := utils.HashPassword(req.Password)
-	log.Info("reached here mf again")
 	if err != nil {
 		log.Warn("error hashing password")
 		http.Error(w, "error hashing password"+err.Error(), http.StatusInternalServerError)
@@ -479,4 +479,138 @@ func (g *Guest) DeleteSessionHistoryHandler(w http.ResponseWriter, r *http.Reque
 	w.Header().Add("Content-Type", "text/plain")
 	message := "successfully deleted all sessions history for the user"
 	w.Write([]byte(message))
+}
+
+func (g *Guest) OauthHandler(w http.ResponseWriter, r *http.Request) {
+	url := g.config.AuthCodeURL("helloo world", oauth2.AccessTypeOffline)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func (g *Guest) OauthCallbackHandler(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+
+	// Exchanging the code for an access token
+	t, err := g.config.Exchange(context.Background(), code)
+	if err != nil {
+		log.Warn("internal error fetching response from the googleapi")
+		http.Error(w, "internal error fetching response from the googleapi", http.StatusInternalServerError)
+		return
+	}
+
+	// Creating an HTTP client to make authenticated request using the access key.
+	// This client method also regenerate the access key using the refresh key.
+	client := g.config.Client(context.Background(), t)
+
+	// Getting the user public details from google API endpoint
+	UserInfo, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Closing the request body when this function returns.
+	// This is a good practice to avoid memory leak
+	defer UserInfo.Body.Close()
+
+	type Goauth struct {
+		Id            string `json:"id"`
+		Email         string `json:"email"`
+		VerifiedEmail bool   `json:"verified_email"`
+		Name          string `json:"name"`
+		GivenName     string `json:"given_name"`
+		FamilyName    string `json:"family_name"`
+		Picture       string `json:"picture"`
+		Locate        string `json:"locale"`
+	}
+
+	var jsonUserData Goauth
+	if err = json.NewDecoder(UserInfo.Body).Decode(&jsonUserData); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	user, err := g.DB.GetUserByEmail(context.TODO(), jsonUserData.Email)
+	if err == sql.ErrNoRows {
+		password, err := utils.GenerateRandomString(16)
+		if err != nil {
+			log.Warn("error producing random string for google password:", err.Error())
+			http.Error(w, "intenral error producing password for google user.", http.StatusInternalServerError)
+			return
+		}
+		hashedPassword, err := utils.HashPassword(password)
+		if err != nil {
+			log.Warn("intenral error hashing password for google user")
+			http.Error(w, "internal error producing password for google user.", http.StatusInternalServerError)
+			return
+		}
+		var arg = db.AddAndVerifyUserParams{
+			Name:     jsonUserData.Name,
+			Email:    jsonUserData.Email,
+			Password: hashedPassword,
+		}
+		sessionUser, err := g.DB.AddAndVerifyUser(context.TODO(), arg)
+		if err != nil {
+			log.Warn("error adding user for google user:", err.Error())
+			http.Error(w, "internal error adding user for google user.", http.StatusInternalServerError)
+			return
+		}
+
+		var sessionArg = db.AddSessionParams{
+			UserID:    sessionUser.ID,
+			IpAddress: utils.GetClientIPString(r),
+			UserAgent: utils.GetUserAgent(r),
+		}
+		session, err := g.DB.AddSession(context.TODO(), sessionArg)
+		if err != nil {
+			log.Warn("error adding session for google user:", err.Error())
+			http.Error(w, "user added successfully. Unable to add session internal error", http.StatusOK)
+			return
+		}
+		sessions.SetSessionCookie(w, session.ID.String())
+
+		var resp struct {
+			Data    db.AddAndVerifyUserRow `json:"data"`
+			Message string                 `json:"message"`
+		}
+		resp.Data = sessionUser
+		resp.Message = "user has been successfully authenticated and logged in."
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
+	} else if err != nil {
+		log.Warn("internal error fetching user from GetUserByEmail and != sql.ErrNoRows and err != nil", err.Error())
+		http.Error(w, "error fetching user data to check if the user already exists.", http.StatusInternalServerError)
+		return
+	}
+	if !user.EmailVerified {
+		verifiedUser, err := g.DB.VerifyUserByID(context.TODO(), user.ID)
+		if err != nil {
+			log.Warn("internal server error verifying user on google authentication:", err.Error())
+			http.Error(w, "internal server error  verifying user and setting sessionID cookie.", http.StatusInternalServerError)
+			return
+		}
+		user.EmailVerified = verifiedUser.EmailVerified
+		user.UserVerified = verifiedUser.UserVerified
+		user.UpdatedAt = verifiedUser.UpdatedAt
+	}
+
+	var sessionArg = db.AddSessionParams{
+		UserID:    user.ID,
+		IpAddress: utils.GetClientIPString(r),
+		UserAgent: utils.GetUserAgent(r),
+	}
+	session, err := g.DB.AddSession(context.TODO(), sessionArg)
+	if err != nil {
+		log.Warn("unable to add goolge auth user session:", err.Error())
+		http.Error(w, "unable to add google auth user session", http.StatusInternalServerError)
+		return
+	}
+	sessions.SetSessionCookie(w, session.ID.String())
+	w.Header().Set("Content-Type", "application/json")
+	var resp struct {
+		Data    db.GetUserByEmailRow `json:"data"`
+		Message string               `json:"message"`
+	}
+	resp.Data = user
+	resp.Message = "user logged in successfully and added session cookie."
+	json.NewEncoder(w).Encode(resp)
 }
--- internal/guest/routes.go
+++ internal/guest/routes.go
@@ -3,13 +3,33 @@ package guest
 import (
 	"net/http"
 
+	"github.com/amankhys/multi_vendor_ecommerce_go/pkg/envname"
 	"github.com/amankhys/multi_vendor_ecommerce_go/repository"
 	"github.com/amankhys/multi_vendor_ecommerce_go/repository/db"
+	env "github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 var dbConn = repository.NewDBConfig()
 var DB = db.New(dbConn)
-var g = Guest{DB: DB}
+
+var envM, _ = env.Read(".env")
+var clientId = envM[envname.GoogleClientID]
+var clientSecret = envM[envname.GoogleSecretKey]
+
+var conf = &oauth2.Config{
+	ClientID:     clientId,
+	ClientSecret: clientSecret,
+	RedirectURL:  "http://localhost:7777/auth/callback",
+	Scopes:       []string{"email", "profile"},
+	Endpoint:     google.Endpoint,
+}
+
+var g = Guest{
+	DB:     DB,
+	config: conf,
+}
 
 func RegisterRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("GET /home", g.HomeHandler)
@@ -20,8 +40,8 @@ func RegisterRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("POST /user_signup_otp", g.UserSignUpOTPHandler)
 	mux.HandleFunc("POST /seller_signup_otp", g.SellerSignUpOTPHandler)
 
-	// mux.HandleFunc("GET /google_login", g.GoogleLoginPageHandler)
-	// mux.HandleFunc("POST /google_login", g.GoogleLoginHandler)
+	mux.HandleFunc("GET /auth/login", g.OauthHandler)
+	mux.HandleFunc("GET /auth/callback", g.OauthCallbackHandler)
 
 	mux.HandleFunc("GET /logout", g.LogoutHandler)
 	mux.HandleFunc("DELETE /delete_all_sessions", g.DeleteSessionHistoryHandler)
