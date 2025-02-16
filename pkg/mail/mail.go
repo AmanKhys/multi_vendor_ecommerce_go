7fb79917c8a236aded9424f753394d74cf0751a5
AmanKhys <amankhys2112@gmail.com>
2025-02-16 16:29:20 +0530

    add SendForgotOTPMail() to mail package; delete session.go, db.go in utils package as it is unused and sessions package exist;

--- pkg/mail/mail.go
+++ pkg/mail/mail.go
@@ -37,12 +37,47 @@ func SendOTPMail(otp int, expires_at time.Time, recepientMail string) error {
 	var recepients []string
 	message := fmt.Sprintf("From: %s\r\n", smtpMail) +
 		"To: " + recepientMail + "\r\n" +
-		"Subject: OTP Verification\r\n" +
+		"Subject: Forgot Password OTP Verification\r\n" +
 		"MIME-Version: 1.0\r\n" +
 		"Content-Type: text/plain; charset=\"utf-8\"\r\n" +
 		"\r\n" + // Empty line separates headers from body
 		fmt.Sprintf("Dear User,\n\n"+
 			"Your One-Time Password (OTP) for verification is: %d\n\n"+
+			"This OTP will expire on: %s\n"+
+			"Time remaining: %s\n\n"+
+			"Please do not share this OTP with anyone.\n\n"+
+			"If you did not request this, please ignore this email.\n\n"+
+			"Best regards,\nToy Stores Ecom",
+			otp,
+			expires_at.Format("Monday, 02 Jan 2006, 03:04 PM MST"), // More readable date format
+			time.Until(expires_at).Round(time.Second).String())     // Time left rounded to seconds
+
+	recepients = append(recepients, recepientMail)
+	err = smtp.SendMail(smtpServer, auth, smtpMail, recepients, []byte(message))
+	return err
+}
+
+func SendForgotOTPMail(otp int, expires_at time.Time, recepientMail string) error {
+	envM, err := env.Read(".env")
+	if err != nil {
+		return err
+	}
+	smtpServer := envM[envname.SmtpServer]
+	smtpMail := envM[envname.SmtpEmail]
+
+	auth, err := returnAuth()
+	if err != nil {
+		return err
+	}
+	var recepients []string
+	message := fmt.Sprintf("From: %s\r\n", smtpMail) +
+		"To: " + recepientMail + "\r\n" +
+		"Subject: OTP Verification\r\n" +
+		"MIME-Version: 1.0\r\n" +
+		"Content-Type: text/plain; charset=\"utf-8\"\r\n" +
+		"\r\n" + // Empty line separates headers from body
+		fmt.Sprintf("Dear User,\n\n"+
+			"Your One-Time Password (OTP) to enter forget password is: %d\n\n"+
 			"This OTP will expire on: %s\n"+
 			"Time remaining: %s\n\n"+
 			"Please do not share this OTP with anyone.\n\n"+
